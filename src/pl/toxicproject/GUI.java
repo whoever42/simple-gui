097a14773bff72d3fe0643c09a4d9b8db336a4ad
whoever42 <34209143+whoever42@users.noreply.github.com>
2021-05-01 13:18:21 +0300

    Clean UI for testing

--- src/pl/toxicproject/GUI.java
+++ src/pl/toxicproject/GUI.java
@@ -1,36 +1,22 @@
 package pl.toxicproject;
 
-import javax.print.DocFlavor;
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 
-public class GUI implements ActionListener {
+public class GUI {
 
     private JFrame frame;
-    private JLabel label;
     private JPanel panel;
-    int count = 0;
 
     public GUI() {
 
         frame = new JFrame();
 
-        JButton button = new JButton("- - - Click - - -");
-        button.addActionListener(this);
-
-        label = new JLabel("Number of clicks " + count);
-
         panel = new JPanel();
-        panel.setBorder(BorderFactory.createEmptyBorder(30, 30, 30, 30));
-        panel.setLayout(new GridLayout(0, 1));
-        panel.add(button);
-        panel.add(label);
+        panel.setBorder(BorderFactory.createEmptyBorder(30, 90, 30, 30));
 
         frame.add(panel, BorderLayout.CENTER);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setTitle("Clicking");
         frame.pack();
         frame.setVisible(true);
 
@@ -40,9 +26,4 @@ public class GUI implements ActionListener {
         new GUI();
     }
 
-    @Override
-    public void actionPerformed(ActionEvent e) {
-        count++;
-        label.setText("Number of clicks " + count);
-    }
 }
